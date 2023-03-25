7bff6f56d252e02567fe0778c58d6f51e97fb5e7
wappon_28_dev <wappon_28_dev@ruru.be>
2023-03-25 17:01:09 +0900

    :alembic: デプロイ実験中...

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Hono } from "hono";
+import { todo } from "./todo/api";
 
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello Hono!"));
+app.route("/api/todo", todo);
 
 export default app;
